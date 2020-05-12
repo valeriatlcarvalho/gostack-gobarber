efbf9d5afcf4fc62ca678453e17c1bf5792bacb8
Valéria Todorovski Lucht de  Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-05-12 00:58:34 -0300

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.use(routes);
+
+app.listen(3333, () => console.log('Server started on port 3333'));
