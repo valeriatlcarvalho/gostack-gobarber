0d297b68f080038ed23a5f96421f90b9ff2a3805
Valéria Todorovski Lucht de Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-06-24 23:00:19 -0300

    Finalização da tela de logon - Teclado empurra conteúdo quando aberto - Ao clicar fora do teclado ocorre o efeito padrão de cada OS

--- mobile/src/components/Button/index.tsx
+++ mobile/src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { RectButtonProperties } from 'react-native-gesture-handler';
 import { Container, ButtonText } from './styles';
 
 interface ButtonProps extends RectButtonProperties {
-  chidlren: string;
+  children: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
