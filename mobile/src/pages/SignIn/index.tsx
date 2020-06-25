893ec79be4e39006c0387af645ce5483791a7732
Valéria Todorovski Lucht de Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-06-24 22:39:36 -0300

    Tela de SignIn - Importação de fontes externas - Configuração de ícones - Configuração de rotas

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Button, Input } from '../../components';
+
+import logoImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
